583e40f90bc18307532f25b102486745349aa47d
rousselm4 <rousselm4@gmail.com>
2015-07-31 11:55:01 -0400

    Unmerged commit on branch edit charts.

--- js/main.js
+++ js/main.js
@@ -1,17 +1,16 @@
 $().ready(function () {
-    var onDatabaseLoaded = function (dataReader) {
-        //new nbOccurrences_LevelAgent(dataReader);
-        new nbOccurrences_LevelVariation(dataReader);
-        //new ButterflyEffect(dataReader);
-        //new countEvolution_AllKeyBits(dataReader);
-        //new countEvolutions_AllAgentLevels(dataReader);
-        //new KeyRepetition(dataReader);
-        //new Nist(dataReader);  
-    };
+
+
 
     var onFileSelected = function () {
-        //var dataReader = DataReader(Layout.getSelectedFile(), onDatabaseLoaded);
-        var dataReader = DataReader_1(onDatabaseLoaded);
+        var dataReader = DataReader();
+        new nbOccurrences_LevelAgent(dataReader);
+        new nbOccurrences_LevelVariation(dataReader);
+        new ButterflyEffect(dataReader);
+        new countEvolution_AllKeyBits(dataReader);
+        new countEvolutions_AllAgentLevels(dataReader);
+        new KeyRepetition(dataReader);
+        new Nist(dataReader);
     };
     Layout.init(onFileSelected);
     Layout.show();

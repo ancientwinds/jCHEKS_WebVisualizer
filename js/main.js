c7ad73ad1f837e8f01f54b261c9c0fd900e54f2d
Michael Roussel <rousselm4@gmail.com>
2015-08-03 14:11:39 -0400

    Remove useless log.

--- js/main.js
+++ js/main.js
@@ -4,17 +4,11 @@ $().ready(function () {
 
         var i = 0;
         //new DistanceEvolution(dataReader);
-        console.log(i++);
         new nbOccurrences_LevelAgent(dataReader);
-        console.log(i++);
         new nbOccurrences_LevelVariation(dataReader);
-        console.log(i++);
         new ButterflyEffect(dataReader);
-        console.log(i++);
         new countEvolution_AllKeyBits(dataReader);
-        console.log(i++);
         new countEvolutions_AllAgentLevels(dataReader);
-        console.log(i++);
         new Nist(dataReader);
     };
     Layout.init(onFileSelected);

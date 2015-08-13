b20d2c773cc523720fbd69e31299bb00c29322bd
Michael Roussel <rousselm4@gmail.com>
2015-08-13 12:54:53 -0400

    Add BarChartSidebar.js

--- js/tests/countEvolutions/AllAgentLevel.js
+++ js/tests/countEvolutions/AllAgentLevel.js
@@ -2,14 +2,35 @@
 
         var id = "allLevelAgent";
         Layout.addTab(id, "AllLevelAgent");
-
-        Chart.BarChart(dataReader.getAllLevelAgent(), {
+        var barChartSidebar = BarChartSidebar(id);
+        var config = {
             height: Layout.getContainerHeight(),
             width: Layout.getContainerWidth(),
             target: id + "Svg",
             yAxisTitle: "System",
             xAxisTitle: "Evolutions",
             chartTitle: "Evolutions to see all agent levels"
-        });
+        }
 
+        var chart = Chart.BarChart(dataReader.getAllLevelAgent(), config);
+
+
+        function updateConfig() {
+            barChartSidebar.updateConfigs(config);
+        }
+
+        function updateChart() {
+            chart.update(dataReader.getAllLevelAgent(), config);
+            barChartSidebar.updateStats(chart.getStats());
+        }
+
+        var update = function () {
+            updateConfig();
+            updateChart();
+        }
+
+        var sideBarContents = [];
+        sideBarContents.push(barChartSidebar.getHTML());
+        sideBarContents.push($("<button>").text("update").click(update));
+        Layout.setSidebarContent(id, sideBarContents);
     }

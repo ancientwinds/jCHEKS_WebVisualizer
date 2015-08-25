1dfb1f0b9b9c6b53f89dff8dbfd37bdf0a5e5e7c
Michael Roussel <rousselm4@gmail.com>
2015-08-25 12:48:34 -0400

    Prepare for refactor to add limit

--- php/db/db.php
+++ php/db/db.php
@@ -2,25 +2,27 @@
 class DatabaseManager {
     var $db = null;
 
-    function __construct($name, $limit = "" , $limitedRow) {
-        $this->limitedRow = $limitedRow;
+    function __construct($name, $limit = "" , $limitedRow, $xLimit, $yLimit, $limitedColumn) {
         $this->baselimit = $limit;
-        if($this->limitedRow != ""){
-            $this->limit = "AND ".$this->limitedRow . " < ".$limit. " ";
-        } else{
-            $this->limit = "LIMIT ".$limit. " ";
-        }
+
+        $this->xLimit = $xLimit;
+        $this->yLimit = $yLimit;
+        $this->limitedColumn = $limitedColumn;
+
         $this->db = new PDO("sqlite:databases/" . $name);
         $this->db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
         ini_set('memory_limit', '256M');
         set_time_limit(300);
     }
+
     function secureAlpha($tableName){
         return (preg_match("/^[A-Za-z0-9_-]+$/",trim($tableName)))? trim($tableName) : null;
     }
+
     function secureNumeric($limit){
         return (ctype_digit(trim($limit)))? trim($limit) : null;
     }
+
     function getDataForASystemFromTableInDatabase($table, $systemId){
         if(!$this->secureNumeric($this->baselimit)){echo "Invalid limit.";}
         $statement = $this->db->prepare("SELECT * FROM ".$table." WHERE chaotic_system_id='".$this->secureAlpha($systemId)."' ".$this->limit);
@@ -31,7 +33,7 @@ class DatabaseManager {
     
     function getDataOfAllSystemsFromTableInDatabase($table){
         if(!$this->secureNumeric($this->baselimit)){echo "Invalid limit.";}
-        $statement = $this->db->prepare("SELECT * FROM ".$this->secureAlpha($table)." ".$this->limit);
+        $statement = $this->db->prepare("SELECT * FROM ".$this->secureAlpha($table)." ".$this->xLimit);
         $statement->setFetchMode(PDO::FETCH_ASSOC);
         if(!$statement->execute()) return null;
         return $statement->fetchAll();
@@ -39,8 +41,8 @@ class DatabaseManager {
     
     function getOverallOccurenceData($table){
         if(!$this->secureNumeric($this->baselimit)){echo "Invalid limit.";}
-        $this->limit = str_replace("LIMIT","",$this->limit);
-       $statement = $this->db->prepare("SELECT * FROM '".$this->secureAlpha($table)."' GROUP BY chaotic_system_id, groupIndex");
+        $this->xLimit = str_replace("LIMIT","",$this->xLimit);
+        $statement = $this->db->prepare("SELECT * FROM '".$this->secureAlpha($table)."' GROUP BY chaotic_system_id, groupIndex");
         $statement->setFetchMode(PDO::FETCH_ASSOC);
         if(!$statement->execute()) return null;
         return $statement->fetchAll();
@@ -48,7 +50,7 @@ class DatabaseManager {
     
     function getSystemsNamesInTable($table){
         if(!$this->secureNumeric($this->baselimit)){echo "Invalid limit.";}
-        $statement = $this->db->prepare("SELECT DISTINCT chaotic_system_id FROM ".$this->secureAlpha($table)." ".$this->limit);
+        $statement = $this->db->prepare("SELECT DISTINCT chaotic_system_id FROM ".$this->secureAlpha($table)." ".$this->xLimit);
         $statement->setFetchMode(PDO::FETCH_ASSOC);
         if(!$statement->execute()) return null;
         return $statement->fetchAll();
